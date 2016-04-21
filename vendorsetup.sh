d81126fda44dc8c36a73f1ebea60fc556424f023
grace5921 <androidlover5842@gmail.com>
2016-04-21 21:54:36 +0530

    use patch_fix command to apply patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ function kyleve
  mk_timer mka bacon -j4
 }
 
-function compile_error
+function patch_fix
 {
 echo "patching cm-11.0 sources"
 time patch -p1 <device/samsung/kyleve/patches/patch.diff
